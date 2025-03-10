3f2aa3a5440280aa384fba3e10f57ae103c03774
YUNI0107 <amy123chou@gmail.com>
2025-03-10 22:50:22 +0800

    chore: add github yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/playcanvas-apple-hunt/' : '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
